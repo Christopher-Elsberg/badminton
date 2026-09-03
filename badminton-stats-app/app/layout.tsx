bca27b2f7ccc9cbad34a6ab47d3c184dffb09839
Christopher-Elsberg <chris10damian@gmail.com>
2026-09-03 11:07:56 +0200

    Add service worker registration component

--- badminton-stats-app/app/layout.tsx
+++ badminton-stats-app/app/layout.tsx
@@ -17,3 +17,16 @@ export default function RootLayout({
     </html>
   );
 }
+"use client";
+
+import { useEffect } from "react";
+
+export default function ServiceWorkerRegistration() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js");
+    }
+  }, []);
+
+  return null;
+}
